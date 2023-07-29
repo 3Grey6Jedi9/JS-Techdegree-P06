edaf59093deeaa54abd65d0556983c67d6c752cd
Darth Vader <danieltarancon@gmail.com>
2023-07-29 17:32:58 +0200

    29 1733

--- app.js
+++ app.js
@@ -21,7 +21,73 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Setting up my routes
 
 
+// Index route to render the "Home" page with the locals set to data.projects
+app.get('/', (req, res) => {
 
+
+    res.render('index', {projects: data.projects})
+
+});
+
+
+// About route to render the "About" page
+
+app.get('/about', (req, res) => {
+
+    res.render('about');
+
+
+});
+
+
+// Dynamic "project" route based on the id of the project
+
+app.get('/project/:id', (req, res)=>{
+
+    const projectId = parseInt(req.params.id); // Getting the project ID from the URL
+    const project = data.projects.find((proj) => proj.id === projectId); // Finding the project with the matching ID
+    if (!project) {
+
+    // If project not found, render a 404 page or handle the error as you prefer
+
+        res.render('404');
+
+    } else {
+
+        // Render the customized version of the Pug project template with data specific to the project
+
+        res.render('project', {project});
+
+    }
+
+});
+
+// Starting the server
+
+
+const port = 3000;
+
+app.listen(port, () => {
+
+    console.log(`Server started on port ${port}`);
+
+});
+
+
+
+// 404 handler for undefined routes
+
+app.use((req, res, next) =>{
+
+    const error = new Error('Page not found'); // A new Error object with the error message
+    error.status = 404; // Set the status code for the error (404 for "Not Found")
+    console.log('Error: ${error.message} - Status: ${error.status}'); // logging the error message and status
+    next(error); // Passing the error to the next error-handling middleware
+
+});
+
+
+// Global error handler for server errors
 
 
 
