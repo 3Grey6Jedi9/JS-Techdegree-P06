22f620dfadd2212a7b25a6a8a595efa0c73837af
Darth Vader <danieltarancon@gmail.com>
2023-07-31 17:05:45 +0200

    31 1705

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,47 @@
-
 'use strict';
 
 /**
  * Handle mobile menu functionality to hide/reveal sidebar on mobile layouts
  */
-const body = document.querySelector('body');
+const wrapper = document.querySelector('.wrapper');
+const sidebar = document.getElementById('my-info');
 let headerBtnClicked = false;
 
-document.querySelector('#menu-icon').addEventListener('click', e => {
-  !headerBtnClicked ? body.style.transform = 'translateX(300px)' : body.style.transform = 'translateX(0px)';
-  return headerBtnClicked = !headerBtnClicked;
+document.querySelector('#menu-icon').addEventListener('click', () => {
+  if (!headerBtnClicked) {
+    // Add content of the sidebar
+    sidebar.innerHTML = `
+      <div class="sidebar-info-box">
+        <br>
+        <div class="thumbnail-box">
+          <img class="thumbnail" src="https://placehold.it/550x350" alt="">
+        </div>
+        <div class="dev-intro-box">
+          <h5>Daniel Mula Tarancón</h5>
+          <p>I am a very mysterious person. I do many interesting things from coding to predicting the financial markets.</p>
+          <p>I am so awesome because I dance like a pro.</p>
+          <a href="/about">Learn More &rarr;</a>
+        </div>
+      </div>
+    `;
+
+    // Display the sidebar
+    sidebar.style.display = 'block';
+
+    // Collapse the layout to make space for the sidebar
+    wrapper.classList.add('collapsed');
+  } else {
+    // Hide the sidebar
+    sidebar.style.display = 'none';
+
+    // Expand the layout to its original state
+    wrapper.classList.remove('collapsed');
+  }
+
+  // Toggle the button state
+  headerBtnClicked = !headerBtnClicked;
 });
+
+
 
 
